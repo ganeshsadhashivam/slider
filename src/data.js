2d49ac2e32501fb40defbb41b2ffcb1c9fd9754b
ganesh <ganeshsadhashivam@outlook.com>
2022-12-12 12:31:57 +0530

    Slider Review

---
+++ src/data.js
@@ -0,0 +1,53 @@
+const people = [
+  {
+    id: 1,
+    name: "ganesh",
+    job: "web developer",
+    image:
+      "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDJ8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=60",
+    text: "I am very Much Excited To Learn New Technologies From FreeCodeCamp",
+    quote: "Your time is limited, so don’t waste it living someone else’s life",
+  },
+  {
+    id: 2,
+    name: "April",
+    job: "Fashion Designer",
+    image:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZmFjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=60",
+    text: "I am a Fashion Designer ",
+    quote:
+      "If you want to find the secrets of the universe, think in terms of energy, frequency and vibration",
+  },
+  {
+    id: 3,
+    name: "Alina",
+    job: "artist",
+    image:
+      "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+    text: "I am Artist ",
+    quote:
+      "If you can't explain it simply, you don't understand it well enough.",
+  },
+  {
+    id: 4,
+    name: "Kristi",
+    job: "Finance",
+    image:
+      "https://images.unsplash.com/photo-1580465446361-8aae5321522b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+    text: "I am Finance Advisor",
+    quote:
+      "Failure will never overtake me if my determination to succeed is strong enough",
+  },
+  {
+    id: 5,
+    name: "Jake",
+    job: "Youtuber",
+    image:
+      "https://images.unsplash.com/photo-1548213238-0da7521bd6e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+    text: "I am Traveller",
+    quote:
+      "Take up one idea. Make that one idea your life; dream of it; think of it; live on that idea. Let the brain, the body, muscles, nerves, every part of your body be full of that idea, and just leave every other idea alone. This is the way to success, and this is the way great spiritual giants are produced.",
+  },
+];
+
+export default people;
